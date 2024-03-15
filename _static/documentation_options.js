3c2cd0518fad597578e774926de8ac3a8b5e64cd
azzamh15 <azzamh15@users.noreply.github.com>
2024-03-15 19:49:35 +0000

    deploy: f88f03bb011b5213d3c6a8e3c781a924c59d1c36

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g3386073',
+    VERSION: '0.1.dev1+gf88f03b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
